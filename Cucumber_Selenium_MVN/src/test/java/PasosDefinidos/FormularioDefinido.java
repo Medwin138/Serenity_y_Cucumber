1581d9575476da8d31cfc9533461f09c5104147d
Miller Edwin laguna trujillo <milleredwin85@gmail.com>
2022-03-18 23:07:26 -0500

    Creación de Asserts
    Creación de Asserts

--- Cucumber_Selenium_MVN/src/test/java/PasosDefinidos/FormularioDefinido.java
+++ Cucumber_Selenium_MVN/src/test/java/PasosDefinidos/FormularioDefinido.java
@@ -3,10 +3,14 @@ package PasosDefinidos;
 import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
+
+
 
 public class FormularioDefinido {
 
@@ -26,10 +30,22 @@ public class FormularioDefinido {
     @Cuando("registro mis datos")
     public void registro_mis_datos() {
 
+      driver.findElement(By.id("userName")).sendKeys("Miller Laguna");
+      driver.findElement(By.id("userEmail")).sendKeys("milleredwin85@gmail.com");
+      driver.findElement(By.id("currentAddress")).sendKeys("Pruebas de automatizacion");
+      driver.findElement(By.id("permanentAddress")).sendKeys("Pruebas para ampliar el conocimiento");
     }
 
     @Entonces("se muestran los datos en pantalla")
     public void se_muestran_los_datos_en_pantalla() {
+        driver.findElement(By.xpath("//button[@id=\'submit\']")).click();
+        String informacionObtenida=driver.findElement(By.id("name")).getText();
+        String informacionEsperada= "Name:Miller Laguna";
+        System.out.println("informacion Obtenida:" +informacionObtenida);
+        Assert.assertEquals(informacionEsperada,informacionObtenida);
+        driver.quit();
+
+
 
     }
 
